6edf5e208c36d8d40ab65002db55d5cdb7401ed5
BenBenXD <LOllipopXDD@hotmail.com>
2016-11-23 10:50:56 -0600

    trees_shrubs class commit

---
+++ src/ense374lab/animals/trees_shrubs.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ense374lab.animals;
+
+/**
+ *
+ * @author BenBen
+ */
+public class trees_shrubs {
+private static final int MAX_AGE;
+ 
+public int age;
+public Location location;
+private boolean alive;
+private boolean hunger; 
+            
+public void trees_shrubs(int age, Location location){
+    
+}
+public void death(){
+    
+}
+public Location location(){
+    
+}
+    
+}
