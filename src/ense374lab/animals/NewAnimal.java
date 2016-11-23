76bd3685c1e7d6d6cfa1701afbd07f4cf1fd6413
BenBenXD <LOllipopXDD@hotmail.com>
2016-11-23 10:27:45 -0600

    first commit

---
+++ src/ense374lab/animals/NewAnimal.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ense374lab.animals;
+
+/**
+ *
+ * @author BenBen
+ */
+public class NewAnimal {
+    private String  name;
+    private String type;
+    private int age;
+    public Location location;
+    public boolean hunger;
+    public String foodType;
+    
+    
+public NewAnimal(String n, int ag, void breed, void death, void location, void hunger){
+    
+}
+public String name(){
+    
+}
+private String Type(){
+    
+}
+public int age(int a){
+    
+}
+public void increaseInAge(){
+    
+}
+public void death(){
+    
+}
+public void breed(){
+    
+}
+public void location(){
+    
+}
+public void Hunger(){
+    
+}
+public void hunt(){
+    
+}
+public String Food(){
+    
+}
+public int addAnimal(){
+    
+}
+public list addAnimalList(){
+    
+}
+    
+    
+    
+}
