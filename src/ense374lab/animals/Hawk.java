035a80cbf97ba88381ca2129415e3d2f5ff9aa64
BenBenXD <LOllipopXDD@hotmail.com>
2016-11-23 10:44:16 -0600

    7th animal commit

---
+++ src/ense374lab/animals/Hawk.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ense374lab.animals;
+
+/**
+ *
+ * @author BenBen
+ */
+public class Hawk {
+private static final int MAX_AGE;
+private static final int BREED;
+private static final int MAX_HUNGER;
+private static final int MAX_BIRTHS;
+ 
+public int age;
+private String foodType;
+public Location location;
+private boolean alive;
+private boolean hunger; 
+            
+public void Hawk(int age, Location location, boolean hunger){
+    
+}
+public void death(){
+    
+}
+public void hunger(){
+    
+}
+public Location location(){
+    
+}
+public boolean checkBreed(){
+    
+}
+public int breed(){
+    
+}
+public void move(){
+    
+}
+    
+}
