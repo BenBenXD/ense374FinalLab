3d2affc0dda1393e39570daade812e00e31a5b4c
BenBenXD <LOllipopXDD@hotmail.com>
2016-12-04 16:44:17 -0600

    Location & World update commit

---
+++ src/ense374lab/animals/World.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ense374lab.animals;
+
+/**
+ *
+ * @author BenBen
+ */
+public class World {
+public NewAnimal animal;
+public int x;
+public int y;
+public NewAnimal addAnimal(){
+    
+}
+public NewAnimal getAnimal(NewAnimal an){
+    return an;
+}
+public Location map(int x,int y){
+    
+}
+public static enum direction {
+    
+NORTH, SOUTH, EAST, WEST
+};  
+
+}
